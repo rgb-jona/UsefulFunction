5618d34e2be5d87d555016c46f49d215bde6045d
rgb-jona <60848007+rgb-jona@users.noreply.github.com>
2020-02-09 15:45:14 +0100

    Create RecursiveSearch.py

---
+++ RecursiveSearch.py
@@ -0,0 +1,18 @@
+def RecursiveSearch(string, searchString):
+    """This function returns all appearances of searchString in string and returns them as list"""
+    stringIndex = string.find(searchString)
+    if stringIndex >= 0:
+        # search for further appearances of searchString in string by recursively calling the RecursiveSearch funktion
+        # with trimmed string from stringIndex+1 to end to prevent finding same string position as before again
+        resultList = RecursiveSearch(string[stringIndex + 1:], searchString)
+
+        # since the string was trimmed the stored indices need to be corrected
+        for counter in range(0,len(resultList)):
+            resultList[counter] += stringIndex + 1
+
+        # insert is used instead of append to ensure that the indices sorted from lowest to highest
+        resultList.insert(0,stringIndex)
+        return resultList
+    else:
+        # returning this empty array so it can be filled with the results of the function calls before
+        return []
